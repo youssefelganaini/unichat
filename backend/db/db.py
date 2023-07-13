9bf15412dd3679c2eebd35d15d27905ca517dc34
youssefelganaini <youssef.elganaini@tum.de>
2023-07-13 22:48:14 +0200

    database configuration

--- backend/db/db.py
+++ backend/db/db.py
@@ -8,6 +8,35 @@ cur = conn.cursor()
 
 cur.execute(
     """
-    CREATE TABLE IF NOT EXISTS
+    CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY
+    name TEXT,
+    email TEXT,
+    keywords TEXT,
+    language TEXT,
+    country TEXT
+    );
     """
 )
+
+
+def addUser(name, email, keywords, language, country):
+    try:
+        query = "INSERT INTO users (name, email, keywords, language country) VALUES(%s, %s, %s, %s, %s)"
+        cur.execute(query, (name, email, keywords, language, country))
+    except (Exception, psycopg2.Error) as error:
+        print("Error in adding user operation", error)
+
+
+def deleteUser(email):
+    query = "DELETE from users WHERE email = %s"
+    cur.execute(query, email)
+
+
+def update_email(new_email):
+    query = ""
+
+
+conn.commit()
+cur.close()
+conn.close()
